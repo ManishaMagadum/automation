ffdd785c70838ba01118a741488fa64aa8c77c42
ManishaMagadum <manishamagadum96@gmail.com>
2023-02-22 23:34:05 +0530

    first tc

---
+++ src/webTest/FirstTest.java
@@ -0,0 +1,25 @@
+package webTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.edge.EdgeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstTest {
+
+	public static void main(String[] args)
+	{
+		//Launch Chrome
+		WebDriver driver=new ChromeDriver();
+		
+		WebDriver driver1=new FirefoxDriver();
+				
+	    WebDriver driver2=new EdgeDriver();
+		
+		
+		
+	}
+
+}
